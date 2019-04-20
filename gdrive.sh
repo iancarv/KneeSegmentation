c88b930a31e9de3ba91fb9f1e9fc8e435df3ceac
Ian Carvalho <iancarv@gmail.com>
2019-04-20 14:29:13 -0400

    util script for downloading files from google drive

---
+++ gdrive.sh
@@ -0,0 +1,10 @@
+if [ "$1" == "" ] || [ $# -gt 1 ]; then
+        echo "Please prodivide the file id for download"
+        exit 0
+
+
+ggID="$1"
+ggURL='https://drive.google.com/uc?export=download'  
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"  
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"  
+curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}" 
