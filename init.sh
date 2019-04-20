294f31c3c4d2a1c312f35da4a965bbd95899b1d1
Ian Carvalho <iancarv@gmail.com>
2019-04-20 13:25:20 -0400

    Init shell script

---
+++ init.sh
@@ -0,0 +1,11 @@
+# Check vritual env? $VIRTUAL_ENV
+
+git submodule update --init --recursive
+cd ./Mask_RCNN
+echo "Installing Mask RCNN requirements"
+pip install -r requirements.txt
+echo "Installing Mask RCNN"
+python setup.py install
+
+
+cd ../
